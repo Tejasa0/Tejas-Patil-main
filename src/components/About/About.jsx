fb9cf4f8f6f0a8a289effd4a89c64f07506c7f63
Tejas Patil <tejaspatila0@gmail.com>
2025-09-21 21:44:36 +0530

    Updating resume link
    Modified   src/components/About/About.jsx
    
    Signed-off-by: Tejas Patil <tejaspatila0@gmail.com>

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1g7yBW2YXSQhZcQ1PxYXtTyX--7YsFva7/view?usp=sharing"
+            href="https://drive.google.com/file/d/16jg3DM55UPm0lofvzUBDCl2CjsngviI3/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
